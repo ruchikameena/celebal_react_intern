47916d679aee74f34ef3ea6ac4ad7010d15b9cbb
ruchikameena <ruchika01m@gmail.com>
2025-07-13 10:43:32 +0530

    backend is deployed and connected with frontend

--- week_7-8/frontend/src/api.js
+++ week_7-8/frontend/src/api.js
@@ -1,6 +1,5 @@
 //it is used to fetch the data from the backend
 import axios from "axios";
-console.log("REACT_APP_BACKEND_URL: ",`${process.env.REACT_APP_backend_url}/api/v1`)
 const API = axios.create({
     baseURL: `${process.env.REACT_APP_backend_url}/api/v1`,
 });
