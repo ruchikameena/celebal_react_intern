d9e088475bdb9d9ba2ad6316b055ec1df83ac768
ruchikameena <ruchika01m@gmail.com>
2025-07-13 10:16:05 +0530

    backend is delpoyed

--- week_7-8/frontend/src/api.js
+++ week_7-8/frontend/src/api.js
@@ -1,7 +1,7 @@
 //it is used to fetch the data from the backend
 import axios from "axios";
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://celebal-react-intern.onrender.com/api/v1",
 });
 
 export default API;
